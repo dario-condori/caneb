09cd33f949109c18ff2990108c12a0caff3e7d77
Darío Condori <dariocondori100@hotmail.com>
2025-04-02 00:30:59 -0400

    Actualizacion de anuarios

--- resources/views/portal/anuarios.blade.php
+++ resources/views/portal/anuarios.blade.php
@@ -15,14 +15,98 @@
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="post mb-20">
                             <div class="post-entry">
-                                <img src="{{ asset('portal/images/caneb/anuario2020-1.png') }}" alt="guia de exportacion">
+                                <img src="{{asset('/storage/memorias/anuario-2023-1.png')}}" width="90%" alt="Anuario 2023">
                             </div>
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-4 col-lg-4">
                         <div class="post mb-20">
                             <div class="post-entry">
-                                <img src="{{ asset('portal/images/caneb/anuario2020-2.png') }}" alt="guia de exportacion">
+                                <img src="{{asset('/storage/memorias/anuario-2023-2.png')}}" width="90%" alt="Anuario 2023">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <p class="module-title font-alt">Memoria Institucional 2023</p>
+                            </div>
+                            <div class="module-subtitle font-serif">
+                                <a class="btn btn-success btn-circle btn-round" href="{{ asset('/storage/memorias/anuario-2023.pdf') }}" target="_blank">
+                                    <i class="fa fa-cog fa-spin"></i> Descargar 2023
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row multi-columns-row post-columns">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2022-1.png')}}" width="90%" alt="Anuario 2022">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2022-2.png')}}" width="90%" alt="Anuario 2022">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <p class="module-title font-alt">Memoria Institucional 2022</p>
+                            </div>
+                            <div class="module-subtitle font-serif">
+                                <a class="btn btn-success btn-circle btn-round" href="{{ asset('/storage/memorias/anuario-2022.pdf') }}" target="_blank">
+                                    <i class="fa fa-cog fa-spin"></i> Descargar 2022
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row multi-columns-row post-columns">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2021-1.png')}}" width="90%" alt="Anuario 2021">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2021-2.png')}}" width="90%" alt="Anuario 2021">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <p class="module-title font-alt">Memoria Institucional 2021</p>
+                            </div>
+                            <div class="module-subtitle font-serif">
+                                <a class="btn btn-success btn-circle btn-round" href="{{ asset('/storage/memorias/anuario-2021.pdf') }}" target="_blank">
+                                    <i class="fa fa-cog fa-spin"></i> Descargar 2021
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row multi-columns-row post-columns">
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2020-1.png')}}" width="90%" alt="Anuario 2020">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-4 col-lg-4">
+                        <div class="post mb-20">
+                            <div class="post-entry">
+                                <img src="{{asset('/storage/memorias/anuario-2020-2.png')}}" width="90%" alt="Anuario 2020">
                             </div>
                         </div>
                     </div>
@@ -32,11 +116,14 @@
                                 <p class="module-title font-alt">Memoria Institucional 2020</p>
                             </div>
                             <div class="module-subtitle font-serif">
-                                <a class="btn btn-success btn-circle btn-round" href="{{ asset('portal/documentos/memoria-institucional-caneb-2020.pdf') }}" target="_blank"><i class="fa fa-cog fa-spin"></i> Descarga gratuita</a>
+                                <a class="btn btn-success btn-circle btn-round" href="{{ asset('/storage/memorias/anuario-2020.pdf') }}" target="_blank">
+                                    <i class="fa fa-cog fa-spin"></i> Descargar 2020
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
+                
                 <div class="row">
                     <div class="col-sm-6 col-sm-offset-3">
                         
